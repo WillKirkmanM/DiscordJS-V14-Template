a2aa411653bea9d86fdbacd8069582c14c29d8a4
Will <willkirkmanmoller@gmail.com>
2022-08-02 17:28:53 +0100

    😒 Extra Space (Error)

--- handler/commandHandler.js
+++ handler/commandHandler.js
@@ -1,5 +1,5 @@
 const ascii = require("ascii-table");
-const commandCheck = require(' ./../utils/commandCheck');
+const commandCheck = require('./../utils/commandCheck');
 const table = new ascii().setHeading('Command', 'Load Status');
 
 module.exports = async (err, files, client) => {
